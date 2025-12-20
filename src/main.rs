093d287aacec89476212365fa10b57d6898d9d27
Konstantinos Lyrakis <24938740+lyrakisk@users.noreply.github.com>
2025-12-20 22:03:09 +0100

    Define Ball & Position

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, (spawn_ball, spawn_camera))
+        .run();
 }
+
+fn spawn_ball(mut commands: Commands) {
+    println!("Spawning ball...");
+    commands.spawn(Ball);
+}
+
+fn spawn_camera(mut commands: Commands) {
+    println!("Spawning camera...");
+    commands.spawn(Camera2d);
+}
+
+#[derive(Component, Default)]
+#[require(Transform)]
+struct Position(Vec2);
+
+#[derive(Component)]
+#[require(Position)]
+struct Ball;
