0090ef6a3c0398db8ef12518dd8af97f485e9ed9
Konstantinos Lyrakis <24938740+lyrakisk@users.noreply.github.com>
2025-12-24 13:27:36 +0100

    Remove unused imports

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,8 +1,6 @@
 use bevy::prelude::*;
 
-use crate::components::ball::*;
 use crate::components::position::*;
-use crate::components::velocity::*;
 
 pub fn project_positions(mut positionables: Query<(&mut Transform, &Position)>) {
     for (mut transform, position) in &mut positionables {
